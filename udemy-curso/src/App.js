7061508ef4702101057cc1276a5ebc3db025a158
Rafael Morales <rmorales.bps@gmail.com>
2019-07-10 14:39:00 -0400

    Sección 3-19: State III  Propagación del state

--- udemy-curso/src/App.js
+++ udemy-curso/src/App.js
@@ -47,7 +47,13 @@ class Contador extends Component{
     },1000)
   }
   render(){
-    return <span>{this.state.contador}</span>
+    return <ContadorNumero numero={this.state.contador}/>
+  }
+}
+
+class ContadorNumero extends Component{
+  render(){
+    return <span>{this.props.numero}</span>
   }
 }
 
